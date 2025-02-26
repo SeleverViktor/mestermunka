7dac6deceefa2b26fa23e09487867f12bf214e3b
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-02-26 10:33:03 +0100

    kezdet

---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from  'react'
+
+function Navbar(){
+    return(
+
+    <div>
+        <nav className="navbar">
+
+        <div className="navbar-conteiner">
+            <Link to="/" className="navbar-logo"></Link>
+        
+        </div>
+
+
+        </nav>
+    </div>
+    )
+}
+export default Navbar

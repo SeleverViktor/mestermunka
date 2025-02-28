0b5f4537da23591a7999491233507fae029e249c
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-02-28 11:28:30 +0100

    navbarfolytatasgombbal

--- src/assets/Button.jsx
+++ src/assets/Button.jsx
@@ -1,3 +1,37 @@
 import React from "react";
 import './Button.css';
 import {Link} from 'react-router-dom';
+
+const STYLES =['btn--primary','btn--outline'];
+
+const SIZES=['btn--medium','btn--large'];
+
+export const Button =({
+    children, 
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+
+
+})=>{
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+    ? buttnStyle
+    :STYLES[0]
+
+    const checkButtonSize =SIZES.includes(buttonSize) ? buttonSize: SIZES[0]
+
+    return(
+       < Link to='/sign-up' className='btn-mobile'>
+        <button
+        className={`btn ${checkButtonStsyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+        >
+            {children}
+        </button>
+
+        </Link>
+     )
+};
+   

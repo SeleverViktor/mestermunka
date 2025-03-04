a8da75c22bb37c1b523699bbf5a7c3ae5e5cc683
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-03-04 12:25:19 +0100

    megvan anav

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from  'react'
 import {Link} from 'react-router-dom'
 import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar(){
     const[click,setClick]=useState(false);
@@ -25,7 +26,7 @@ function Navbar(){
         <nav className="navbar">
          <div className="navbar-conteiner">
             <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3"></i>
+             <i className="fab fa-typo3"></i>TRVL
             </Link>
            < div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
@@ -61,3 +62,4 @@ function Navbar(){
     );
 }
 export default Navbar
+//valamiért eltünt a sign up gomb :(

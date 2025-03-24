b79ed8ef5e3707d1be6ef1cb083b014b04aabcae
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-03-24 23:18:28 +0100

    ujoldalakeskinezetuk

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './assets/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // A Switch helyett Routes-ot használunk v6-ban
 import './App.css';
 import Home from './assets/pages/Home';
+import Services from './assets/pages/Services';
+import Products from './assets/pages/Products';
+import SignUp from './assets/pages/SignUp';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,8 +16,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes> {/* Routes a v6-ban */}
-          <Route path="/" exact Component=
-          {Home}/>
+          <Route path='/' exact component={Home}/>
+          <Route path ='/services' component={Services}/>
+          <Route path ='/products' component={Products}/>
+          <Route path ='/sign-up' component={SignUp}/>
         </Routes>
       </Router>
     </>
--- src/assets/Cards.jsx
+++ src/assets/Cards.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './Carditem';
+import './Cards.css';
 
 function Cards(){
     return(
@@ -8,7 +9,38 @@ function Cards(){
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem/>
+                        <CardItem
+                        src="images/img-9.jpg"
+                        text="megolom magam, ha igy haladok"
+                        label='Adventure'
+                        path='/services'
+                        />
+                           <CardItem
+                        src="images/img-2.jpg"
+                        text="nem viccelek"
+                        label='luxury'
+                        path='/services'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                        src="images/img-3.jpg"
+                        text="már készülök ugrani"
+                        label='sigmamood'
+                        path='/services'
+                        />
+                           <CardItem
+                        src="images/img-4.jpg"
+                        text="három-kettő-e.."
+                        label='adrenaline'
+                        path='/services'
+                        />
+                            <CardItem
+                        src="images/img-8.jpg"
+                        text="vége"
+                        label='relax'
+                        path='/services'
+                        />
                     </ul>
                 </div>
             </div>
